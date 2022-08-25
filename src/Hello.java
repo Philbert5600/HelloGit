2d52a655d3ad0cb6810cb22cce500a7e5795925c
grant <grant@Grants-Laptop.unl.edu>
2022-08-25 12:02:37 -0500

    Grant Kerrigan's Addition

--- src/Hello.java
+++ src/Hello.java
@@ -2,8 +2,11 @@
 public class Hello {
 	
 	public static void main(String args[]) {
-		System.out.println("Jack Williames")
-		System.out.println("Computer Science")
+		System.out.println("Jack Williames");
+		System.out.println("Computer Science");
+		System.out.println();
+		System.out.println("Grant Kerrigan");
+		System.out.println("Computer Science");
 		
 	}
 }
